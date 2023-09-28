200d5c0f8929c37f3e25168495f0bd403f170948
Nikolay <frenzymind@mail.ru>
2023-09-29 00:56:40 +0800

    feat: remove eslint webpack plugin, small modification for ts

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,18 +4,14 @@ import { BuildOptions } from './types/config'
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   const typescriptLoader = {
-    test: /\.tsx?$/,
+    test: /\.ts$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   }
 
   const cssLoader = {
     test: /\.s[ac]ss$/i,
-    use: [
-      options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-      'css-loader',
-      'sass-loader',
-    ],
+    use: [options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
   }
 
   return [typescriptLoader, cssLoader]
